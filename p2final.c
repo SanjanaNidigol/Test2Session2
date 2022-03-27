89d4ecaed451ee6aad547404bb224c4cbd930b83
SanjanaNidigol <95744225+SanjanaNidigol@users.noreply.github.com>
2022-03-27 04:42:09 +0000

    form a tri

--- p2final.c
+++ p2final.c
@@ -11,14 +11,12 @@ void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float
 }
 int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3)
 {
-float a,b,c;
-  a=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
-  b=sqrt(pow((x3-x2),2)+pow((y3-y2),2));
-  c=sqrt(pow((x1-x3),2)+pow((y1-y3),2));
-  if((a+b)>c && (b+c)>a && (c+a)>b)
-    return 0;
-  else
+float a;
+  a=0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2));
+  if(a==0)
     return 1;
+  else
+    return 0;
 }
 void output(float x1, float y1, float x2, float y2,float x3, float y3, int istriangle)
 {
